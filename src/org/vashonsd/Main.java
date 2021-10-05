6b9d82956fa14b3bbc32d5250b5807c95ba321f4
stamat.alwyn <alwynhawk@gmail.com>
2021-10-05 10:49:23 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,13 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(squareOf(7));
+    }
+    public static int squareOf(int n) {
+
+        return n * n;
+    }
+}
